fceea5d552ace17553f9551d125ec0e13f1d7f3c
yhyuntak <yhyuntak@gmail.com>
2025-06-22 13:33:53 +0900

    [ADD] shadcn 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -22,6 +23,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <div className="flex min-h-svh felx-col items-center justify-center">
+          <Button>Click me</Button>
+        </div>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
